455ffeaf8b46fa128695a1d7db3fcf1bf8a9b6f1
Augusto Romero <geragusto@hotmail.com>
2018-03-14 00:15:51 -0300

    InitialCommit
    Commit Init Server Express

---
+++ index.js
@@ -0,0 +1,21 @@
+const express= require("express");
+const http= require("http");
+const morgan=require("morgan");
+
+const host="localhost";
+const port=3000;
+
+const app=express();
+app.use(morgan("dev"));
+app.use(express.static(__dirname+"/public"));
+app.use((req,res,next)=>{
+    res.statusCode=404;
+    res.setHeader("Content-Type","text/html");
+    res.end("<html><body><h2>No se ha encontrado la pagina</h2></body></html>");
+});
+
+const server=http.createServer(app);
+
+server.listen(port, host, ()=>{
+    console.log(`Server running at http://${host}:${port}`);
+})
